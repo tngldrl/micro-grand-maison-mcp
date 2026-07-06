edf67330e230dcfe63413fee5a5b5603750c1372
user <mail@mail.com>
2026-07-06 19:47:09 +0900

    restrict to localhost

--- server.py
+++ server.py
@@ -593,4 +593,4 @@ def chat(req: ChatRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("server:app", host="127.0.0.1", port=8001, reload=True)
